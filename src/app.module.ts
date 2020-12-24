382387c65273223e7b0630e0e06ddad344841e0c
Farah <kacem.farah@insat.u-carthage.tn>
2020-12-24 20:10:52 +0100

    configured database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [
+      "dist/**/*.entity{.ts,.js}"
+    ],
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
