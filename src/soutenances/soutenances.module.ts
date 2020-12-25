6604a33dcdbdfe4c541f76d63d1f976e066adca1
Farah <kacem.farah@insat.u-carthage.tn>
2020-12-25 01:31:03 +0100

    Session creation and mapping to database works

--- src/soutenances/soutenances.module.ts
+++ src/soutenances/soutenances.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SessionController } from './controllers/session/session.controller';
 import { SoutenanceController } from './controllers/soutenance/soutenance.controller';
+import { SessionEntity } from './entities/session.entity';
+import { SessionService } from './services/session.service';
 
 @Module({
-  controllers: [SessionController, SoutenanceController]
+  controllers: [SessionController, SoutenanceController],
+  providers: [SessionService],
+  imports: [
+    TypeOrmModule.forFeature([
+      SessionEntity
+    ])
+  ]
 })
 export class SoutenancesModule {}
